8fee807fcfd2e690b9dd9ac61ed37acd05727010
James Hentig <jhentig@crcfe01.crc.nd.edu>
2019-10-16 13:15:45 -0400

    Updated with all tables and final.code.sh

---
+++ final.code.sh
@@ -0,0 +1,31 @@
+#usage: compiles all reference genes into a singular fiel
+#cat hsp70* > hsp70refs
+#cat mcrA* > mcrArefs
+
+#usage: aligns reference sequences
+#./muscle -in hsp70refs -out alignment.hsp70
+#./muscle in mcrArefs -out alignment.mcrA 
+
+#usage: create a fuzzy picture of the gene 
+#./Private/bin/bin/hmmbuild --amino ./bioinformatics_project2019/alignment.mcrA.hmmbuild ./bioinformatics_project2019/alignment.mcrA
+#./Private/bin/bin/hmmbuild --amino ./bioinformatics_project2019/alignment.hsp70.hmmbuild ./bioinformatics_project2019/alignment.hsp70  
+
+#usage: compares hmmbuild "fuzzy" picture to the proteome sequence and generates a table with the number of WP lines being matches
+#./Private/bin/bin/hmmsearch --tblout proteome.x.mcrA.table ./bioinformatics_project2019/alignment.mcrA.hmmbuild ./bioinformatics_project2019/proteomes/proteome_0x.fasta
+#./Private/bin/bin/hmmsearch --tblout proteome.x.hsp70.table ./bioinformatics_project2019/alignment.hsp70.hmmbuild ./bioinformatics_project2019/proteomes/proteome_0x.fasta
+
+#mkdir output_tables
+#cd output_tables
+#mkdir mcrA_tables
+#mkdir hsp70_tables
+#cd ..
+
+
+cd ./bioinformatics_project2019/
+for file in proteomes/proteome_*.fasta; do
+        name=`echo $file | cut -d "." -f 1 | cut -d "/" -f 2`
+        echo "$name"
+	~/Private/bin/bin/hmmsearch --tblout output_tables/mcrA_tables/${name}.mcrA.table alingment.mcrA.hmmbuild $file
+	~/Private/bin/bin/hmmsearch --tblout output_tables/hsp70_tables/${name}.hsp70.table alingment.hsp70.hmmbuild $file
+done
+
